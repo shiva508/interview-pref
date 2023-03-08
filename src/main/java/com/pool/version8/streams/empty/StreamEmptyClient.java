2ee25a4c07bd9ce758cf4820f01bb2136f5a5d90
shiva508 <dasarishiva1@gmail.com>
2023-03-08 08:02:47 +0530

    generate,iterate

--- src/main/java/com/pool/version8/streams/empty/StreamEmptyClient.java
+++ src/main/java/com/pool/version8/streams/empty/StreamEmptyClient.java
@@ -5,5 +5,6 @@ import java.util.stream.Stream;
 public class StreamEmptyClient {
     public static void main(String[] args) {
         Stream<String> stream = Stream.empty();
+        System.out.println(stream.toString());
     }
 }
